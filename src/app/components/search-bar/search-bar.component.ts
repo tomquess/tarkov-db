7bdc2a5b6f10cb0d9a85a473aac87d89472deea9
Tomasz Czarzbon <tomques11@gmail.com>
2023-01-22 17:48:19 +0100

    delete services

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,7 +1,8 @@
 
-import { Component, OnInit, Output } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -12,9 +13,9 @@ export class SearchBarComponent implements OnInit {
 
 
   constructor(private router: Router) { }
-
   ngOnInit(): void {
   }
+
 
   onSubmit(form: NgForm) {
     this.router.navigate(['search', form.value.search]);
